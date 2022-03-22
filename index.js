f6039da2017c99eab3e099f90a25e6a1ef323092
hh1207 <96813406+hh1207@users.noreply.github.com>
2022-03-21 23:28:59 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require('mysql2');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Happy1207?",
+    database: "crud_contact"
+});
+
+
+app.get("/", (req,res) =>{
+ 
+    res.send("Hello World");
+  
+});
+ 
+app.get("/contacts", (req,res) =>{
+    db.query("SELECT * FROM contact_db", (err,result) => {
+        if (err) {
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
+  
+});
+
+
+app.post("/create", (req, res) => {
+    console.log(req.body);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  
+
+  db.query(
+    "INSERT INTO contact_db (firstName, lastName, email) VALUES (?,?,?)",
+    [firstName,lastName,email],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+
+app.listen(3001, () => {
+    console.log("Listening on port 3001. Hello World");
+})
